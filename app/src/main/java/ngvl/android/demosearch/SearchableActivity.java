29a2711eaf27adf81cee366bd67a78654f66c47c
rajatbeck <rajatmarcelb@gmail.com>
2016-09-25 12:11:15 -0700

    initial commit

---
+++ app/src/main/java/ngvl/android/demosearch/SearchableActivity.java
@@ -0,0 +1,80 @@
+package ngvl.android.demosearch;
+
+import android.app.SearchManager;
+import android.content.AsyncQueryHandler;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    private static final String TAG = SearchableActivity.class.getSimpleName();
+    private MyHandler mHandler;
+
+    private TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+        txt = (TextView) findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            txt.setText("Searching by: " + query);
+            Uri uri = CitySuggestionProvider.CONTENT_URI;
+            Log.d(TAG, String.valueOf(uri));
+            mHandler = new MyHandler(this);
+            mHandler.startQuery(0, null, uri, null, null, new String[]{query}, null);
+
+        } else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            Log.d(TAG, String.valueOf(intent.getData()));
+            mHandler = new MyHandler(this);
+            mHandler.startQuery(0, null, intent.getData(), null, null, null, null);
+        }
+    }
+
+    public void updateText(String text) {
+        txt.setText(text);
+    }
+
+    static class MyHandler extends AsyncQueryHandler {
+        private static final String TAG = MyHandler.class.getSimpleName();
+        // avoid memory leak
+        WeakReference<SearchableActivity> activity;
+
+        public MyHandler(SearchableActivity searchableActivity) {
+            super(searchableActivity.getContentResolver());
+            activity = new WeakReference<>(searchableActivity);
+        }
+
+        @Override
+        protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
+            super.onQueryComplete(token, cookie, cursor);
+            if (cursor == null || cursor.getCount() == 0) return;
+
+            Log.d(TAG, String.valueOf(cursor.getCount()));
+            cursor.moveToFirst();
+
+            long id = cursor.getLong(cursor.getColumnIndex(BaseColumns._ID));
+            String text = cursor.getString(cursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_1));
+            long dataId = cursor.getLong(cursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_INTENT_DATA_ID));
+
+            cursor.close();
+
+            if (activity.get() != null) {
+                activity.get().updateText("onQueryComplete: " + id + " / " + text + " / " + dataId);
+            }
+        }
+    }
+
+    ;
+}
